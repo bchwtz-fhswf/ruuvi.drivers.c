0d399daadf40c3a7b6023bcb5da73b6584a077d4
caos <sysexect@gmail.com>
2022-01-23 22:02:36 +0100

    added fdb initialization

--- src/tasks/ruuvi_task_sensor.c
+++ src/tasks/ruuvi_task_sensor.c
@@ -5,8 +5,14 @@
 #include "ruuvi_interface_log.h"
 #include "ruuvi_task_flash.h"
 #include "ruuvi_task_sensor.h"
+#include "flashdb.h"
+#include "macronix_flash.h"
+#include "ruuvi_task_flashdb.h"
+
 
 #include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 #ifndef TASK_SENSOR_LOG_LEVEL
 #define TASK_SENSOR_LOG_LEVEL RI_LOG_LEVEL_DEBUG
@@ -25,6 +31,55 @@ static inline void LOGD (const char * const msg)
 static inline void LOGHEX (const uint8_t * const msg, const size_t len)
 {
     ri_log_hex (TASK_SENSOR_LOG_LEVEL, msg, len);
+}
+
+/* KVDB object */
+static struct fdb_kvdb kvdb = {0};
+
+/* Key-Value database initialization
+*
+*       &kvdb: database object
+*       "env": database name
+*   partition: The flash partition name base on FAL. Please make sure it's in FAL partition table.
+* &default_kv: The default KV nodes. It will auto add to KVDB when first initialize successfully.
+*        NULL: The user data if you need, now is empty.
+*/
+rd_status_t init_fdb(rt_sensor_ctx_t * const sensor) {
+    struct fdb_blob blob;
+    uint8_t sensor_config_fdb_enabled = 0;
+
+    /* default KV nodes */
+    struct fdb_default_kv_node default_kv_table[] = {
+        {"sensor_config_fdb_enabled", &sensor_config_fdb_enabled, sizeof(sensor_config_fdb_enabled)}};
+
+    struct fdb_default_kv default_kv;
+    default_kv.kvs = default_kv_table;
+
+    char* partition_name;
+    partition_name = malloc(strlen("sensor_")+strlen(sensor->sensor.name)+strlen("_config"));
+    strcpy(partition_name, "sensor_");
+    strcat(partition_name, sensor->sensor.name);
+    strcat(partition_name, "_config");
+    fdb_err_t result = fdb_kvdb_init(&kvdb, "env", partition_name, &default_kv, NULL);
+    rt_macronix_high_performance_switch(false); //resetting high-power mode in case of factory reset
+    free(partition_name);
+    if (result == FDB_NO_ERR)
+    {
+
+        fdb_kv_get_blob(&kvdb, "sensor_config_fdb_enabled", fdb_blob_make(&blob, &sensor_config_fdb_enabled, sizeof(sensor_config_fdb_enabled)));
+        if (blob.saved.len > 0 && sensor_config_fdb_enabled)
+        {
+            // activate logging
+            // return app_enable_sensor_logging(NULL, false);
+            return RD_SUCCESS;
+        }
+        else
+        {
+        // but return RD_SUCCESS
+        return RD_SUCCESS;
+        }
+    }
+    return RD_ERROR_FATAL;
 }
 
 /** @brief Initialize sensor CTX
@@ -59,6 +114,7 @@ rd_status_t rt_sensor_initialize (rt_sensor_ctx_t * const sensor)
     {
         err_code |= RD_ERROR_NOT_FOUND;
     }
+    err_code |= init_fdb(sensor);
 
     return err_code;
 }
@@ -115,6 +171,7 @@ rd_status_t rt_sensor_load (rt_sensor_ctx_t * const sensor)
     }
     else
     {
+        // TODO: have to set fdb config before this point
         err_code |= rt_flash_load (sensor->nvm_file, sensor->nvm_record,
                                    & (sensor->configuration),
                                    sizeof (sensor->configuration));
